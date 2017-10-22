ee245638a3f5178277f710c347fb2a9d6ac9a09b
Grace Xin <gracexin2003@users.noreply.github.com>
2017-10-22 14:05:38 -0700

    update_3

--- Jewel_Red.java
+++ Jewel_Red.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import android.graphics.Color;
@@ -40,30 +11,26 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 /**
- * In this program, the color sensor is on the right of the stick attached to the servos,
- * with the front being the right side and the back being the left side.
+ * In this program, the side of the robot that is facing the jewels has the color sensor.
+ * The color sensor is supposed to sense the color of the ball on the right side of the podium.
  */
 
 @TeleOp(name="Jewel_Red", group="Linear Opmode")
-//@Disabled
 public class Jewel_Red extends LinearOpMode {
 
-    // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
 
     ColorSensor cs;
     Color c;
 
-    Servo servo;
+    Servo mainServo;
+    Servo seconsServo;
     double servoPosition = 0.0;
 
     DcMotor leftMotor;
     DcMotor rightMotor;
     DcMotor leftBackMotor;
     DcMotor rightBackMotor;
-
-    //This program assumes that the side of the robot that is facing the jewels has the color sensor.
-    //The color sensor should be sensing the jewel on the right side of the separator.
     
     @Override
     public void runOpMode() {
@@ -73,14 +40,17 @@ public class Jewel_Red extends LinearOpMode {
         cs = hardwareMap.colorSensor.get("Color Sensor");
         cs.enableLed(true);
 
-        servo = hardwareMap.servo.get("servo");
-        servo.setPosition(servoPosition);
+        secondServo = hardwareMap.servo.get("servo2");
+        mainServo = hardwareMap.servo.get("servo1");
+        mainServo.setPosition(servoPosition);
 
         waitForStart();
         runtime.reset();
 
-        servoPosition = 0.5;
-        servo.setPosition(servoPosition);
+        servoPosition = 1.0;
+        secondServo.setPosition(servoPosition);
+        sleep(50);
+        mainServo.setPosition(servoPosition);
 
         leftMotor = hardwareMap.dcMotor.get("Left_Motor");
         rightMotor = hardwareMap.dcMotor.get("Right_Motor");
@@ -99,7 +69,9 @@ public class Jewel_Red extends LinearOpMode {
             rightBackMotor.setPower(0.0);
         }
 
-        servoPosition = 0.5;
-        servo.setPosition(servoPosition);
+        servoPosition = 0.0;
+        mainServo.setPosition(servoPosition);
+        sleep(50);
+        secondServo.setPosition(servoPosition);
     }
 }
