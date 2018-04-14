d328740a0a42aea052be39ea40ec351fbe815d9f
Grace Xin <gracexin2003@users.noreply.github.com>
2018-04-14 16:53:14 -0700

    minor changes

--- NewDriveTrain.java
+++ NewDriveTrain.java
@@ -7,22 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
 @TeleOp(name="Drivetrain", group="Linear Opmode")
-//@Disabled
 public class Drivetrain extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
@@ -67,31 +52,19 @@ public class Drivetrain extends LinearOpMode {
             leftBackMotor.setPower(-gamepad1.left_stick_y);
             rightBackMotor.setPower(-gamepad1.right_stick_y);
 
-            if(gamepad2.dpad_up){
-                frontLift.setPower(1);
-            } else {
-                frontLift.setPower(0);
-            }
+            if(gamepad2.dpad_up){frontLift.setPower(1);
+            else frontLift.setPower(0);
 
-            if(gamepad2.dpad_down){
-                frontLift.setPower(-1);
-            } else {
-                frontLift.setPower(0);
-            }
+            if(gamepad2.dpad_down)frontLift.setPower(-1);
+            else frontLift.setPower(0);
 
-            if(gamepad2.y){
-                backLift.setPower(1);
-            } else {
-                backLift.setPower(0);
-            }
+            if(gamepad2.y)backLift.setPower(1);
+            else backLift.setPower(0);
 
-            if(gamepad2.a){
-                backLift.setPower(-1);
-            } else {
-                backLift.setPower(0);
-            }
+            if(gamepad2.a)backLift.setPower(-1);
+            else backLift.setPower(0);
             
-                if(gamepad2.left_bumper) {
+            if(gamepad2.left_bumper) {
                 frontServo.setPosition(0.67);
                 backServo.setPosition(0.10);
             }
@@ -100,8 +73,7 @@ public class Drivetrain extends LinearOpMode {
                 frontServo.setPosition(0.30);
                 backServo.setPosition(0.50);
             }
-
-
+            
             if(gamepad2.right_bumper){
                 leftBackServo.setPosition(0.22);
                 rightBackServo.setPosition(0.39);
